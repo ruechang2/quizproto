8b52cd2262cf23349a7f77ec91b1a21ec1dfd4b5
kotaisayama <i.kouta@yahoo.com.au>
2020-05-19 23:41:56 +0900

    update alzebra

--- englishQuiz/englishQuiz/ViewController.swift
+++ englishQuiz/englishQuiz/ViewController.swift
@@ -14,19 +14,25 @@ class ViewController: UIViewController {
         present(nav, animated: true, completion: nil)
     }
     
+    let A = QuizFixture.test(
+        question: "Which human is fast to run?\n\n1) Joe \n2) Sai \n3) X \n4) Zero",
+        answerCount: 4, answer: 4)
+    
+    
+    let B = QuizFixture.test(
+        question: "Which animal is fast to run?\n\n1) Panda \n2) Elephant \n3) Giraffe \n4) Cheetah",
+        answerCount: 4, answer: 4)
+    
+    let C = QuizFixture.test(
+        question: "How many minutes a day?\n\n1) 1680 min\n2) 2880 min\n3) 1440 min",
+        answerCount: 3, answer: 3)
     
     private func fetchQuizzes() -> [Quiz] {
-        return [
-            QuizFixture.test(
-                question: "Which is red fruit?\n\n1) Banana\n2) Apple\n3) Lemon\n4) Kiwi",
-                answerCount: 4, answer: 2),
-            QuizFixture.test(
-                question: "Which animal is fast to run?\n\n1) Panda \n2) Elephant \n3) Giraffe \n4) Cheetah",
-                answerCount: 4, answer: 4),
-            QuizFixture.test(
-                question: "How many minutes a day?\n\n1) 1680 min\n2) 2880 min\n3) 1440 min",
-                answerCount: 3, answer: 3),
-        ]
+        return[
+        A, B, C
+]
+            
+        
     }
 }
 
